91b4ff7ac9f605ce15d4777284a923c8fe8d4da3
Kaishiyoku <dmp@nfze.de>
2018-05-23 11:12:30 +0200

    closed #1 refactored crawler code

--- app/Console/Commands/CrawlImages.php
+++ app/Console/Commands/CrawlImages.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\Image;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Symfony\Component\DomCrawler\Crawler;
 
 class CrawlImages extends Command
@@ -44,42 +45,27 @@ class CrawlImages extends Command
         $this->line('Crawling images...this will take some time.');
         $this->line('');
 
-        $content = file_get_contents(env('CRAWLER_BASE_URL') . '/post/list');
+        $crawler = new Crawler($this->getListContent());
 
-        $crawler = new Crawler($content);
+        $lastPageNumber = $this->getLastPageNumber($crawler);
+        $pages = collect(range(2, $lastPageNumber));
 
-        $numberOfPages = collect($crawler
-            ->filter('#paginator > a')
-            ->each(function (Crawler $node, $i) {
-                return $node->text();
-            })
-        )->filter(function ($value) {
-            return $value != '>>';
-        })->last();
-
-        $urls = $crawler
-            ->filter('a.thumb')
-            ->each(function (Crawler $node, $i) {
-                return $node->attr('href');
-            });
+        $this->line('Number of pages: ' . $lastPageNumber);
+        $this->line('');
 
         // get first page
-        $this->getImages($urls);
+        $this->line('Page 1');
+
+        $this->getImages(collect($this->getFullImageUris($crawler)));
 
         // get remaining pages
-        for ($i = 2; $i <= $numberOfPages; $i++) {
-            $curContent = file_get_contents(env('CRAWLER_BASE_URL') . '/post/list/' . $i);
+        $pages->each(function ($page) {
+            $this->line('Page ' . $page);
 
-            $curCrawler = new Crawler($curContent);
+            $crawler = new Crawler($this->getListContent($page));
 
-            $curUrls = $curCrawler
-                ->filter('a.thumb')
-                ->each(function (Crawler $node) {
-                    return $node->attr('href');
-                });
-
-            $this->getImages($curUrls);
-        }
+            $this->getImages(collect($this->getFullImageUris($crawler)));
+        });
 
         $timeElapsedInSeconds = microtime(true) - $start;
 
@@ -88,29 +74,69 @@ class CrawlImages extends Command
         $this->line('');
     }
 
-    private function getImages($urls)
+    private function getContent($uri)
     {
-        foreach ($urls as $url) {
-            $externalId = collect(explode('/', $url))->last();
-            $curContent = file_get_contents(env('CRAWLER_BASE_URL') . $url);
+        return file_get_contents(env('CRAWLER_BASE_URL') . $uri);
+    }
 
-            $curCrawler = new Crawler($curContent);
+    private function getListContent($pageNumber = null)
+    {
+        $uri = $pageNumber == null ? '/post/list' : "/post/list/$pageNumber";
+
+        return $this->getContent($uri);
+    }
+
+    private function getLastPageNumber(Crawler $crawler)
+    {
+        return collect($crawler
+            ->filter('#paginator > a')
+            ->each(function (Crawler $node) {
+                return $node->text();
+            })
+        )->filter(function ($value) {
+            return $value != '>>';
+        })->last();
+    }
+
+    private function getFullImageUris(Crawler $crawler)
+    {
+        return $crawler
+            ->filter('a.thumb')
+            ->each(function (Crawler $node) {
+                return $node->attr('href');
+            });
+    }
+
+    private function getExternalIdByUri($uri)
+    {
+        return collect(explode('/', $uri))->last();
+    }
+
+    private function replaceNewLines($content)
+    {
+        return preg_replace("/\n|\t/", '', $content);
+    }
+
+    private function getImages(Collection $uris)
+    {
+        $uris->each(function ($uri) {
+            $externalId = $this->getExternalIdByUri($uri);
+
+            $crawler = new Crawler($this->getContent($uri));
 
             $rating = collect(
-                $curCrawler
+                $crawler
                     ->filterXPath('//table/tr/td')
-                    ->each(function (Crawler $node, $i) {
-                        return preg_replace("/\n|\t/", '', $node->text());
+                    ->each(function (Crawler $node) {
+                        return $this->replaceNewLines($node->text());
                     })
             )->filter(function ($value) {
                 return in_array($value, $this->getRatings());
             })->first();
 
-            $imageNode = $curCrawler
-                ->filter('img#main_image')
-                ->first();
+            $imageNode = $crawler->filter('img#main_image')->first();
 
-            // only save not yet crawled images
+            // only save not yet crawled images, update others
             $image = Image::find($externalId);
 
             $imageUrl = $imageNode->count() > 0 ? env('CRAWLER_BASE_URL') . $imageNode->attr('src') : null;
@@ -131,12 +157,19 @@ class CrawlImages extends Command
                 $image->save();
             }
 
-            $this->line('  #' . $externalId);
-        }
+            $this->verbose(function () use ($externalId) {$this->line('  #' . $externalId);});
+        });
     }
 
     private function getRatings()
     {
         return explode(',', env('CRAWLER_RATINGS'));
+    }
+
+    private function verbose(\Closure $closure)
+    {
+        if ($this->option('verbose')) {
+            $closure();
+        }
     }
 }
