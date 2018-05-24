1094836adfbfaa92d4c1f78eb441ba2119bfdcd8
Kaishiyoku <dmp@nfze.de>
2018-05-24 13:31:49 +0200

    minor layout adjustments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,61 +1,29 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('app')
 
-    <title>{{ env('APP_NAME') }}</title>
-
-    <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="img/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="img/favicon/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="img/favicon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-
-    {!! Html::style('css/app.css') !!}
-
-    {!! Html::script('js/app.js') !!}
-</head>
-<body>
-
-<div class="credits">
-    Astolfo render by <a href="https://ino2206.deviantart.com">Ino2206</a>
-</div>
-
-<div class="container">
-    <div class="row pb-5">
-        <div class="col-sm-6 order-1 order-sm-0">
-            <h1>Welcome ^__^</h1>
-        </div>
-
-        <div class="col-sm-6 order-0 order-sm-1 text-center pb-3">
-            <img src="img/astolfo.png" class="img-fluid" style="max-height: 400px"/>
-        </div>
+@section('content')
+    <div class="credits">
+        Astolfo render by <a href="https://ino2206.deviantart.com">Ino2206</a>
     </div>
 
-    <p>
-        <a href="https://mangas.astolfo.rocks" class="btn btn-dark btn-lg">Manga list</a>
-    </p>
+    <div class="container">
+        <div class="row pb-5">
+            <div class="col-sm-6 order-1 order-sm-0">
+                <h1>Welcome ^__^</h1>
+            </div>
 
-    <h2>Random Astolfo</h2>
+            <div class="col-sm-6 order-0 order-sm-1 text-center pb-3">
+                <img src="img/astolfo.png" class="img-fluid" style="max-height: 400px"/>
+            </div>
+        </div>
 
-    <a href="{{ env('CRAWLER_BASE_URL') }}/post/view/{{ $randomImage->external_id }}">
-        <img src="{{ $randomImage->url }}" class="img-fluid img-thumbnail" style="max-height: 400px;"/>
-    </a>
-</div>
+        <p>
+            <a href="https://mangas.astolfo.rocks" class="btn btn-dark btn-lg">Manga list</a>
+        </p>
 
-</body>
-</html>
+        <h2>Random Astolfo</h2>
+
+        <a href="{{ env('CRAWLER_BASE_URL') }}/post/view/{{ $randomImage->external_id }}">
+            <img src="{{ $randomImage->url }}" class="img-fluid img-thumbnail" style="max-height: 400px;"/>
+        </a>
+    </div>
+@endsection
