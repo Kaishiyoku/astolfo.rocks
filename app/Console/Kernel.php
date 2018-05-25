8b05d12b97da0af3c07524b605f86a295a5637ce
Kaishiyoku <dmp@nfze.de>
2018-05-25 21:14:12 +0200

    fixed console kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\CrawlImages;
+use App\Console\Commands\CrawlTest;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(CrawlImages::class)->dailyAt('07:00');
-        $schedule->command(CrawlImages::class, ['--test'])->dailyAt('03:00');
+        $schedule->command(CrawlTest::class)->dailyAt('03:00');
     }
 
     /**
