2d046c79e1a5294448634ffb05994b11ef0d3300
Kaishiyoku <dmp@nfze.de>
2020-12-27 23:30:22 +0100

    minor code refactorings

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
             <div class="text-xl pt-8 pb-2">Random Astolfo</div>
 
             <a href="{{ env('CRAWLER_BASE_URL') }}/post/view/{{ $randomImage->external_id }}">
-                <img src="{{ $randomImage->url }}" class="img-fluid border border-pink-400 rounded p-1 bg-white" alt="random Astolfo image"/>
+                <img src="{{ url('/api/v1/images/' . $randomImage->external_id . '/data') }}" class="img-fluid border border-pink-400 rounded p-1 bg-white" alt="random Astolfo image"/>
             </a>
         </div>
     </div>
