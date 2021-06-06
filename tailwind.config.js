caeca779680ace0a87df4a73f7e19a44511fa18f
Kaishiyoku <dmp@nfze.de>
2021-06-06 16:15:35 +0200

    updated npm dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  purge: [
-    './resources/views/**/*.blade.php',
-    './resources/css/**/*.css',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Nunito', 'sans-serif'],
-      },
-      maxHeight: {
-        '400': '400px',
-      },
+    mode: 'jit',
+    purge: [
+        './resources/views/**/*.blade.php',
+        './resources/css/**/*.css',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Nunito', 'sans-serif'],
+            },
+            maxHeight: {
+                '400': '400px',
+            },
+        },
     },
-  },
-  variants: {},
-  plugins: [],
+    variants: {},
+    plugins: [],
 }
