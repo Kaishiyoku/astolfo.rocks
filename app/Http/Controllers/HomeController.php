5d90122909537f5f4a05a825492d813a05936706
Kaishiyoku <dmp@nfze.de>
2021-11-06 16:45:10 +0100

    added API documentation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Enums\ImageRating;
 use App\Models\Image;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        Http::get('url')->json();
+
         $randomImage = Image::whereRating(ImageRating::Safe())->inRandomOrder()->first();
 
         return view('welcome', compact('randomImage'));
