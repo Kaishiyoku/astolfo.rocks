9a6966d70dd5d17d7c50056adc9aefac0772353b
Kaishiyoku <dmp@nfze.de>
2021-11-13 13:12:28 +0100

    run duplication check once per day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CheckForDuplicateImages;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//        $schedule->command(SampleCommand::class)->dailyAt('07:00');
+        $schedule->command(CheckForDuplicateImages::class)->dailyAt('04:00');
     }
 
     /**
