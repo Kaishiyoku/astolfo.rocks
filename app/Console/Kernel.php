6221d10d9b9caa9809af214ab3a11f7d51280583
Kaishiyoku <dmp@nfze.de>
2022-09-02 19:30:57 +0200

    adjusted duplicate image check schedule rule (2)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(CheckForDuplicateImages::class)->everyTwoHours();
+        $schedule->command(CheckForDuplicateImages::class)->twiceDaily(4, 16);
     }
 
     /**
