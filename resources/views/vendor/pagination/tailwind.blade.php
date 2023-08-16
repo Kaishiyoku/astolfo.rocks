2120b1179dde960ffa1a0b7f9521bc70e6ba0e74
Shift <shift@laravelshift.com>
2023-08-16 06:20:39 +0000

    Sort Tailwind classes

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -6,13 +6,13 @@
                     {!! __('pagination.previous') !!}
                 </span>
             @else
-                <a href="{{ $paginator->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-400 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 leading-5 rounded-md hover:text-gray-500 dark:hover:text-gray-300 focus:outline-none focus:ring ring-gray-300 focus:border-pink-300 active:bg-gray-100 dark:active:bg-gray-600 active:text-gray-700 dark:text-gray-400 transition ease-in-out duration-150">
+                <a href="{{ $paginator->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-400 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 leading-5 rounded-md hover:text-gray-500 dark:hover:text-gray-300 focus:outline-none focus:ring ring-gray-300 focus:border-pink-300 active:bg-gray-100 dark:active:bg-gray-600 active:text-gray-700 transition ease-in-out duration-150">
                     {!! __('pagination.previous') !!}
                 </a>
             @endif
 
             @if ($paginator->hasMorePages())
-                <a href="{{ $paginator->nextPageUrl() }}" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 dark:text-gray-400 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 leading-5 rounded-md hover:text-gray-500 dark:hover:text-gray-300 focus:outline-none focus:ring ring-gray-300 focus:border-pink-300 active:bg-gray-100 dark:active:bg-gray-600 active:text-gray-700 dark:text-gray-400 transition ease-in-out duration-150">
+                <a href="{{ $paginator->nextPageUrl() }}" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 dark:text-gray-400 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 leading-5 rounded-md hover:text-gray-500 dark:hover:text-gray-300 focus:outline-none focus:ring ring-gray-300 focus:border-pink-300 active:bg-gray-100 dark:active:bg-gray-600 active:text-gray-700 transition ease-in-out duration-150">
                     {!! __('pagination.next') !!}
                 </a>
             @else
@@ -71,7 +71,7 @@
                                         <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-pink-600 dark:text-pink-300 bg-pink-100 dark:bg-pink-600 border border-gray-300 dark:border-gray-700 cursor-default leading-5">{{ $page }}</span>
                                     </span>
                                 @else
-                                    <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 dark:text-gray-400 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 leading-5 hover:text-gray-500 dark:hover:text-gray-300 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-pink-300 active:bg-gray-100 dark:active:bg-gray-600 active:text-gray-700 dark:text-gray-400 transition ease-in-out duration-150" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
+                                    <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 dark:text-gray-400 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 leading-5 hover:text-gray-500 dark:hover:text-gray-300 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-pink-300 active:bg-gray-100 dark:active:bg-gray-600 active:text-gray-700 transition ease-in-out duration-150" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
                                         {{ $page }}
                                     </a>
                                 @endif
