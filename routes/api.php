44bf25168853b1900e2e93b2fd7e7f0be7396247
Kaishiyoku <dmp@nfze.de>
2023-08-16 08:52:08 +0200

    adjusted API route names

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('api')->group(function () {
+Route::middleware('api')->as('api.')->group(function () {
     Route::get('/health_check', [HomeController::class, 'healthCheck'])->name('health_check');
     Route::get('/version', [HomeController::class, 'version'])->name('version');
 
