aa4b2ff01f3e78f7fb6aa28e83adaf649b244996
Kaishiyoku <dmp@nfze.de>
2023-11-16 20:57:21 +0100

    minor adjustments

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,4 @@
             {{ __('Dashboard') }}
         </x-page-header.headline>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 border-b border-gray-200 dark:border-gray-700">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
-    </div>
 </x-app-layout>
