dd93bbba4ecef9eb30cc9cc06181d700484fdfc0
Kaishiyoku <dmp@nfze.de>
2023-11-18 17:31:11 +0100

    adjusted navigation style

--- resources/views/components/icon-link.blade.php
+++ resources/views/components/icon-link.blade.php
@@ -1,5 +1,5 @@
 @props(['href'])
 
-<a href="{{ $href }}" {{ $attributes->merge(['class' => 'inline-flex items-center space-x-1 px-2 py-0.5 rounded-full text-gray-500 hover:text-gray-700 focus:ring focus:ring-1 focus:ring-gray-400 transition']) }}>
+<a href="{{ $href }}" {{ $attributes->merge(['class' => 'inline-flex items-center space-x-1 px-2 py-0.5 rounded-full text-gray-500 hover:text-gray-700 focus:ring-1 focus:ring-gray-400 transition']) }}>
     {{ $slot }}
 </a>
