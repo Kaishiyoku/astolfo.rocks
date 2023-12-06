6711acad76820ff2930b84e54119aa596316db05
Kaishiyoku <dmp@nfze.de>
2023-12-06 09:47:06 +0100

    fixed image edit page

--- resources/views/image/_form_fields.blade.php
+++ resources/views/image/_form_fields.blade.php
@@ -1,7 +1,7 @@
 <div class="mb-4">
     <x-label for="rating" :value="__('validation.attributes.rating')" required/>
 
-    <x-select id="rating" class="block mt-1 w-full" name="rating" :value="old('rating', $image->rating)" :options="getAvailableImageRatingOptions()" required/>
+    <x-select id="rating" class="block mt-1 w-full" name="rating" :value="old('rating', $image->rating?->value)" :options="getAvailableImageRatingOptions()" required/>
 
     <x-validation-error for="rating"/>
 </div>
