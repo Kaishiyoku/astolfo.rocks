e992f2ea11cb69e6af8c6ecaa8ccb6363d25ff6b
Shift <shift@laravelshift.com>
2026-04-21 17:07:10 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Console\Commands\CheckForDuplicateImages;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command(CheckForDuplicateImages::class)->everyTwoHours();
