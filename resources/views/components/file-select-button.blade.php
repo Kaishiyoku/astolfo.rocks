273925c725d8c6b0b9cdaada7f55078ef0056bb5
Kaishiyoku <dmp@nfze.de>
2026-04-21 19:24:33 +0200

    removed `cjstroud/classnames-php`

--- resources/views/components/file-select-button.blade.php
+++ resources/views/components/file-select-button.blade.php
@@ -1,6 +1,6 @@
 @props(['name', 'multiple' => false])
 
-<div x-data="fileSelectButton()" class="{{ classNames('flex items-center space-x-2', $attributes->get('class')) }}">
+<div x-data="fileSelectButton()" @class([$attributes->get('class'), 'flex items-center space-x-2'])>
     <input type="file" id="{{ $name }}" name="{{ $name }}" hidden x-ref="fileInputElement" {{ $attributes->except(['class', 'id']) }} @change="handleChange($event)"/>
 
     <label for="{{ $name }}" class="cursor-pointer inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition">
--- resources/views/components/tabs/link.blade.php
+++ resources/views/components/tabs/link.blade.php
@@ -2,20 +2,12 @@
 
 <a
     href="{{ $href }}"
-    class="{{ classNames('inline-flex items-baseline px-3 py-0.5 rounded-lg transition', [
-        'dark:text-gray-100 bg-white dark:bg-gray-500' => $active,
-        'hover:bg-white/50 dark:hover:bg-gray-500/50' => !$active
-    ]) }}"
+    @class(['inline-flex items-baseline px-3 py-0.5 rounded-lg transition', 'dark:text-gray-100 bg-white dark:bg-gray-500' => $active, 'hover:bg-white/50 dark:hover:bg-gray-500/50' => !$active])
 >
     <span>{{ $slot }}</span>
 
     @if ($badge)
-        <span
-            class="{{ classNames('ml-2 text-xs', [
-                'text-gray-500 dark:text-gray-300' => $active,
-                'text-gray-500 dark:text-gray-400' => !$active,
-            ]) }}"
-        >
+        <span @class(['ml-2 text-xs', 'text-gray-500 dark:text-gray-300' => $active, 'text-gray-500 dark:text-gray-400' => !$active])>
             {{ $badge }}
         </span>
     @endif
