9c0a2e52a8a2ac94daafeb3a936689275e75473d
Shift <shift@laravelshift.com>
2026-04-21 17:07:04 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\ImageManagerServiceProvider::class,
+    App\Providers\ImageHashServiceProvider::class,
 ];
--- config/app.php
+++ config/app.php
@@ -5,22 +5,6 @@ use Illuminate\Support\ServiceProvider;
 
 return [
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\ImageManagerServiceProvider::class,
-        App\Providers\ImageHashServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'ImageManager' => App\Facades\ImageManagerFacade::class,
